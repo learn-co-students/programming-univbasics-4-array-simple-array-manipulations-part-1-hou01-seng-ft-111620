5171cdd0a7a567e95953c51081aaa8a6ffb2224d
Alan sanchez <asanchezhaza@gmail.com>
2020-10-14 05:59:02 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,51 @@
+ 
+def using_push(colors_in_the_rainbow, next_color)
+ 
+    
+    colors_in_the_rainbow.push(next_color)
+    
+ end
+ 
+ 
+ def using_unshift( bouroughs_in_nyc, new_neighborhood )
+  
+   bouroughs_in_nyc.unshift (new_neighborhood)
+ end
+ 
+ 
+ def using_pop (continents)
+   
+continents.pop
+ 
+ 
+  end
+  
+  def pop_with_args (array)
+    array.pop(2)
+  
+  
+  
+  end
+  
+  def using_shift (array)
+    array.shift
+  end
+    
+  
+  
+ def shift_with_args (array)
+   array.shift(2)
+   
+ end
+ 
+ 
+
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+   
